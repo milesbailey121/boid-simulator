078a89496a44c9665a454e08f28a0a3f76ab2c68
milesbailey121 <milesbailey121@gmail.com>
2025-04-09 19:50:17 +0100

    Updated Move command to dyamically caclualte the direction of boids. Added Cohesion, Align and Seperate function and varaibles

--- src/boid.py
+++ src/boid.py
@@ -1,12 +1,7 @@
 import pygame
 import numpy as np
 import random
-
-DEGREE_MIN_MAX = [0,360]
-COLOR_MIN_MAX = [0,255]
-TURNFACTOR = 0.2
-ROTATEFACTOR = 0.01
-MARGIN = 100
+from src.constants import *
 
 class Boid:
     def __init__(self,id):
@@ -15,37 +10,56 @@ class Boid:
         # Generates a random angle in radians(the standard unit of angular measure used in maths) between 0:360
         self.direction = random.uniform(np.radians(DEGREE_MIN_MAX[0]),np.radians(DEGREE_MIN_MAX[1]))
         # Generates random position on screen
-        self.position = np.array([random.uniform(0,self.width),random.uniform(0,self.height)])
+        self.position = np.array([random.uniform(MARGIN,self.width - MARGIN),random.uniform(MARGIN,self.height - MARGIN)])
         # Generates random velocity vector. 
         # Cosine of direction gives velocity component of x-direction 
         # Sine of direction gives velocity component of y-direction
         self.velocity = np.array([np.cos(self.direction),np.sin(self.direction)])
         
         self.acceleration = np.array([0,0])
-        
+        self.coherence_acceleration = np.array([0,0])
+        self.seperate_acceleration = np.array([0,0])
+        self.align_acceleration = np.array([0,0])
+
         self.polygon = np.array([(20,0),(0,5),(0,-5)])
         self.color = ((random.uniform(COLOR_MIN_MAX[0],COLOR_MIN_MAX[1])),(random.uniform(COLOR_MIN_MAX[0],COLOR_MIN_MAX[1])),(random.uniform(COLOR_MIN_MAX[0],COLOR_MIN_MAX[1])))
 
     def move(self):
+        self.acceleration = self.coherence_acceleration + self.seperate_acceleration + self.align_acceleration
+
+        vector = self.velocity + self.acceleration
+
+        self.velocity = (vector) / np.linalg.norm(vector)
+        self.direction = np.arctan2(self.velocity[1],self.velocity[0])
         self.position += self.velocity
+
         # Logic to loop boids around screen
-        if self.position[0] > self.width or self.position[0] < 0:
-            self.position[0] = np.abs(self.position[0] - self.width)
-        if self.position[1] > self.height or self.position[1] < 0:
-            self.position[1] = np.abs(self.position[1] - self.height)
+        # if self.position[0] > self.width or self.position[0] < 0:
+        #     self.position[0] = np.abs(self.position[0] - self.width)
+        # if self.position[1] > self.height or self.position[1] < 0:
+        #     self.position[1] = np.abs(self.position[1] - self.height)
         #TO DO: Implement Boid turning around at boreder 
-        # if self.position[0] < MARGIN:
-        #     self.velocity[0] = self.velocity[0] + TURNFACTOR
-        #     self.direction = self.direction + ROTATEFACTOR
-        # if self.position[0] > self.width - MARGIN:
-        #     self.velocity[0] = self.velocity[0] - TURNFACTOR
-        #     self.direction = self.direction - ROTATEFACTOR
-        # if self.position[1] > self.height - MARGIN:
-        #     self.velocity[1] = self.velocity[1] - TURNFACTOR
-        #     self.direction= self.direction - ROTATEFACTOR
-        # if self.position[1] < MARGIN:
-        #     self.velocity[1] = self.velocity[1] + TURNFACTOR
-        #     self.direction = self.direction + ROTATEFACTOR
+        if self.position[0] < MARGIN:
+            self.velocity[0] = self.velocity[0] + TURNFACTOR
+        if self.position[0] > self.width - MARGIN:
+            self.velocity[0] = self.velocity[0] - TURNFACTOR
+        if self.position[1] > self.height - MARGIN:
+            self.velocity[1] = self.velocity[1] - TURNFACTOR
+        if self.position[1] < MARGIN:
+            self.velocity[1] = self.velocity[1] + TURNFACTOR
+
+    def cohere(self,boids,distances,power,radius):
+        near_boids = [
+            d[1] for d in distances[self.id]
+            if 0 < d[0] < radius and boids[d[1]]
+        ]
+
+    def seperate():
+        pass
+
+    def align():
+        pass
+        
 
     def draw(self,display):
         # https://www.youtube.com/watch?v=a59YQ4qe7mE : 2D rotation matrix
