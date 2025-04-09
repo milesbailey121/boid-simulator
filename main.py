d278006f6ba6b4ba31a4ca0e2d148107bc698bb0
milesbailey121 <milesbailey121@gmail.com>
2025-04-09 20:49:08 +0100

    Created distance calculator between boids

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import pygame
 from pygame.locals import *
-from boid import Boid
+from src.boid import Boid
 from pygame.math import Vector2 as Vector
+from src.constants import *
+import numpy as np
 
 pygame.init()
-BOID_NUM = 100
+
+def distance_between_vectors(vectors):
+	distance_list = [[0] * len(vectors) for _ in range(len(vectors))]
+	for i in range(len(vectors)):
+		distance_list[i][i] = (0, i)
+		for j in range(len(vectors)):
+			if i < j:
+				# Calculate the distance between two vectors.
+				distance = np.linalg.norm(np.subtract(vectors[i], vectors[j]))
+				# Store the calculated distance at index i,j.
+				distance_list[i][j] = (distance, j)
+				distance_list[j][i] = (distance, i)
+	return distance_list
+
 def main(height,width):
     run = True
 
     display = pygame.display.set_mode((height, width))
     pygame.display.set_caption("Boid Simulation")
     clock = pygame.time.Clock()
-
+    
     boids = []
     for i in range(BOID_NUM):
         boids.append(Boid(i))
@@ -21,13 +36,16 @@ def main(height,width):
             if event.type == pygame.QUIT:
                 run = False
                 exit()
-        
+    
         display.fill((0,0,0))
+
+        distances = distance_between_vectors([boid.position for boid in boids]) # Added
         for boid in boids:
+            boid.cohere(boids,distances)
             boid.move()
             boid.draw(display)
 
-        clock.tick(30)
+
         pygame.display.update()
 
 if __name__ == "__main__":
